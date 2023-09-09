1cef1b0ccffab85438b8be12c3cf6c6e73b09185
TomaszPosluszny <tomkposluszny@gmail.com>
2023-09-09 12:24:11 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RealizationsComponent } from './pages/realizations/realizations.component';
+import { ReferenceComponent } from './pages/reference/reference.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    ContactComponent,
+    HomeComponent,
+    RealizationsComponent,
+    ReferenceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/realizations/realizations.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RealizationsComponent } from './realizations.component';
+
+describe('RealizationsComponent', () => {
+  let component: RealizationsComponent;
+  let fixture: ComponentFixture<RealizationsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RealizationsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RealizationsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
